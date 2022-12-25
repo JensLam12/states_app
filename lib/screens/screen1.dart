1dbecea2f080084d862b306ba138a707c9847a7f
JensLam12 <jesterlynn1986@gmail.com>
2022-12-24 16:57:24 -0800

    Added a few changes

--- lib/screens/screen1.dart
+++ lib/screens/screen1.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:states_app/services/user_service.dart';
+
+import '../models/user.dart';
 
 class Screen1 extends StatelessWidget {
   const Screen1({super.key});
@@ -9,7 +12,14 @@ class Screen1 extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Screen 1'),
       ),
-      body: userInfo(),
+      body: StreamBuilder(
+        stream: userService.userStream.stream,
+        builder: (BuildContext context, AsyncSnapshot<User> snapshot) {
+          return snapshot.hasData 
+          ? userInfo(user: snapshot.data!)
+          : const Center( child: Text('There is no information of user') );
+        }
+      ),
       floatingActionButton: FloatingActionButton(
         child: const Icon( Icons.accessibility_new),
         onPressed: () => Navigator.pushNamed(context, 'screen2'),
@@ -19,8 +29,10 @@ class Screen1 extends StatelessWidget {
 }
 
 class userInfo extends StatelessWidget {
+  final User user;
   const userInfo({
-    Key? key,
+    Key? key, 
+    required this.user,
   }) : super(key: key);
 
   @override
@@ -31,17 +43,17 @@ class userInfo extends StatelessWidget {
       padding: const EdgeInsets.all(20.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
-        children: const [
-          Text('General', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold ) ),
-          Divider(),
-          ListTile( title: Text('Name: ') ),
-          ListTile( title: Text('Edad: ') ),
+        children: [
+          const Text('General', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold ) ),
+          const Divider(),
+          ListTile( title: Text('Name: ${user.name}' ) ),
+          ListTile( title: Text('Edad: ${user.age}') ),
 
-          Text('Profesiones', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold ) ),
-          Divider(),
-          ListTile( title: Text('Profesion 1') ),
-          ListTile( title: Text('Profesion 2') ),
-          ListTile( title: Text('Profesion 3') ),
+          const Text('Profesiones', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold ) ),
+          const Divider(),
+          const ListTile( title: Text('Profesion 1') ),
+          const ListTile( title: Text('Profesion 2') ),
+          const ListTile( title: Text('Profesion 3') ),
         ],
       ),
     );
--- lib/screens/screen2.dart
+++ lib/screens/screen2.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:states_app/models/user.dart';
+import 'package:states_app/services/user_service.dart';
 
 class Screen2 extends StatelessWidget {
   const Screen2({super.key});
@@ -15,14 +17,15 @@ class Screen2 extends StatelessWidget {
           children: [
             MaterialButton(
               onPressed: () {
-
+                final newUser = User(name: 'Timmy', age: 35);
+                userService.loadUser(newUser);
               },
               color: Colors.blue,
               child: const Text( 'Make user', style: TextStyle( color: Colors.white) ),
             ),
             MaterialButton(
               onPressed: () {
-
+                userService.updateAge(30);
               },
               color: Colors.blue,
               child: const Text( 'Change age', style: TextStyle( color: Colors.white) ),
